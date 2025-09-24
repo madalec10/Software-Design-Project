111af3d15d9696d8262ca338405063222ff86da4
WhatsLifes <64847243+WhatsLifes@users.noreply.github.com>
2025-09-24 17:10:31 -0500

    created sign up page

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -12,12 +12,12 @@ const LogIn = () => {
                 <h2>Log in</h2>
                 
                 <label for="email">Email:</label>
-                <input type="email" placeholder='Email' name="Email" required maxLength={40}/>
+                <input type="email" placeholder='person123@gmail.com' name="Email" required maxLength={40}/>
                 
                 <label for="password">Password:</label>
-                <input type="password" placeholder='Password' name="Password" required minLength={12} maxLength={16}/>
+                <input type="password" placeholder='ABC123' name="Password" required minLength={12} maxLength={16}/>
                 
-                <Link to="/signup" className='link'>Don't have an account?</Link>
+                <Link to="/sign-up" className='link'>Don't have an account?</Link>
                 
                 <button type="submit">User Log In</button>
                 <button type="submit">Admin Log In</button>

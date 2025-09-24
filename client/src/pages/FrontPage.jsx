0f70b25bf1bffab9777932a6778d76615aedaa21
Ethan B <ethan.bydalek@gmail.com>
2025-09-24 18:06:45 -0500

    css page is being used globally :(
    stinky css
    Will try to make frontpage.css only for frontpage.jsx later

--- client/src/pages/FrontPage.jsx
+++ client/src/pages/FrontPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../FrontPage.css'
+import style from '../FrontPage.module.css'
 
 // is there a way I can import images from a single directory instead of this
 import africaVolunteerImg from "../assets/africa-volunteer.png"
@@ -21,14 +21,14 @@ const FrontPage = () => {
             top right corner (nav bar?) if user signed in */}
             <a href="/account-details" rel="noopener noreferrer">Account Details Page (temporary)</a> <br></br>
             
-            <img src={logoImg} alt="helping hands logo" width="200" height="200"></img>
+            <img src={logoImg} id = "logoImg" alt="helping hands logo" width="200" height="200"></img>
             {/* FIXME: place helping hands title next to the logo */}
             <h1 id = "logoText">Helping Hands</h1>
             
-            <p><em>Nurturing a better future</em></p>
+            <p id = "motto"><em>Nurturing a better future</em></p>
 
             
-            <img src={diversityImg} class = "bigImg" alt="diverse group of people" width="512" height="341"></img>
+            <img src={diversityImg} class = {style.bigImg} alt="diverse group of people" width="512" height="341"></img>
             <br></br>
             <h1>About Us</h1>
             <p>
@@ -53,17 +53,17 @@ const FrontPage = () => {
             </p>
             <h1>Gallery</h1>
             {/* flexbox */}
-            <img src={africaVolunteerImg} class = "gallery" alt="volunteers in africa" width="480" height="250"></img>
-            <img src={africanPeopleImg} class = "gallery" alt="african people" width="466" height="311"></img>
-            <img src={indiaVolunteerImg} class = "gallery" alt="volunteers in india" width="474" height="291"></img>
-            <img src={languageExchangeImg} class = "gallery" alt="language exchange event" width="512" height="341"></img>
-            <img src={plantVolunteerImg} class = "gallery" alt="volunteering by planting plants" width="600" height="315"></img>
+            <img src={africaVolunteerImg} class = {style.gallery} alt="volunteers in africa" width="480" height="250"></img>
+            <img src={africanPeopleImg} class = {style.gallery} alt="african people" width="466" height="311"></img>
+            <img src={indiaVolunteerImg} class = {style.gallery} alt="volunteers in india" width="474" height="291"></img>
+            <img src={languageExchangeImg} class = {style.gallery} alt="language exchange event" width="512" height="341"></img>
+            <img src={plantVolunteerImg} class = {style.gallery} alt="volunteering by planting plants" width="600" height="315"></img>
 
 
             <h1>Lend a Helping Hand Today!</h1>
 
-            <img src={diverseFriendsImg} class = "bigImg" alt="diverse group of friends" with="915" height="400"></img>
-            <p><a href="Login.jsx">Login</a> or <a href="SignUp.jsx">Create an Account</a></p>
+            <img src={diverseFriendsImg} class = {style.bigImg} alt="diverse group of friends" with="915" height="400"></img>
+            <p id = "signInPrompt"><a href="Login.jsx">Login</a> or <a href="SignUp.jsx">Create an Account</a></p>
         </div>
         
 

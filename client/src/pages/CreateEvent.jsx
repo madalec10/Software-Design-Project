93cc30bf268352473f45d3e6b8bece46cb0f5dcb
Alec Lindsey <madalec13@gmail.com>
2025-09-26 12:12:17 -0500

    Merging Bryan's work

--- client/src/pages/CreateEvent.jsx
+++ client/src/pages/CreateEvent.jsx
@@ -1,11 +1,66 @@
 import React from 'react'
- 
+import Select from 'react-select'
+import { useState } from 'react';
+
+import "./../CreateEvent.css"
+
+
+const options = [
+    { value: "Organizing", label: "Organizing" },
+    { value: "Power Tools", label: "Power Tools" },
+    { value: "Teamwork", label: "Teamwork" },
+    { value: "Communication", label: "Communication" },
+    { value: "Time Management", label: "Time Management" }
+];
+
 const CreateEvent = () => {
     console.log("CreateEvent")
- 
+
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    
+    const handleChange = (selectedOptions) => {
+        setSelectedOptions(selectedOptions);
+    };
+
     return(
         <div>
-            Create Event
+            <form className="create-event-form">
+                <h2>Create Event</h2>
+                
+                <label for="name" className="create-event-label">Event Name:</label>
+                <input className="create-event-input" placeholder='Volunteer Clean Up' name="name" required maxLength={100}/>
+                
+                <label for="skills" className="create-event-label">Required Skills:</label>
+                <Select name="skills" 
+                options={options} 
+                isMulti 
+                onChange={handleChange} 
+                value={selectedOptions} 
+                className="create-event-input"
+                required/>
+
+                <label for="urgency" className="create-event-label">Urgency Level:</label>
+                <select name="urgency" required>
+                    <option value="" disabled selected required>Select Urgency Level</option>
+                    <option value="high">Help Necessary</option>
+                    <option value="medium">Help Wanted</option>
+                    <option value="low">Help Would be Appreciated</option>
+                </select>
+
+                <label for="date" className="create-event-label">Event Date and Time:</label>
+                <input type="datetime-local" className="create-event-input" name="date" required/>
+
+                <label for="location" className="create-event-label">Event Location:</label>
+                <input type="text" className="create-event-input" name="location" placeholder='123 parkplace 74029 NY'required/>
+
+                <label for="volunteer-count" className="create-event-label">Number of Volunteers Needed:</label>
+                <input type="number" className="create-event-input" name="volunteer-count" placeholder='1-100' required min="1" max="100" />
+
+                <label for="description" className="create-event-label">Event Description:</label>
+                <textarea className="create-event-input" name="description" placeholder="Describe the event..." required maxLength={500}></textarea>
+
+                <button type="submit">Create Event</button>
+            </form>
         </div>
     )
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
  
+import "./../SignUp.css"
+
 const SignUp = () => {
     console.log("SignUp")
  
     return(
         <div>
-            Sign Up
+            <form className="form">
+                <h2>Sign Up</h2>
+                
+                <label for="email">Email:</label>
+                <input type="email" placeholder='person123@gmail.com' name="Email" required maxLength={40}/>
+                
+                <label for="password">Password:</label>
+                <input type="password" placeholder='ABC123' name="Password" required minLength={12} maxLength={16}/>
+                
+                <label for="confirm password">Confirm Password:</label>
+                <input type="password" placeholder='ABC123' name="Confirm Password" required minLength={12} maxLength={16}/>
+
+                <Link to="/log-in" className='link'>Already have an account?</Link>
+                
+                <button type="submit">Sign Up</button>
+                
+            </form>
         </div>
     )
 }

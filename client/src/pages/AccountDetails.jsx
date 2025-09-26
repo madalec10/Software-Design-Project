7c8b69f32ec756ef404104d1f3a51445f19b3007
Ethan B <ethan.bydalek@gmail.com>
2025-09-26 17:44:07 -0500

    account details drop down menus and entries

--- client/src/pages/AccountDetails.jsx
+++ client/src/pages/AccountDetails.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import style from './AccountDetails.module.css'
+import CreatableSelect from 'react-select/creatable'
+import {useState} from "react";
  
 const AccountDetails = () => {
     console.log("AccountDetails")
  
+    const [fullName, setFullName] = useState("");
+    
     return(
         <div>
             
@@ -12,53 +16,222 @@ const AccountDetails = () => {
             <h3 class = {style.header}>Personal Information</h3>
             
             <span class = {style.field}><strong>Full Name: </strong></span>
-            <span class = {style.value}>Walter Hartwell White Sr.</span>
+            <input
+                class = {style.value}
+                type = "text"
+                value = {fullName}
+                onChange = {(e) => setFullName(e.target.value)}
+                placeholder = "Walter Hartwell White Sr."
+                maxLength = {50}
+            />
+            <span style={{ color: 'red' }}><em>  limit 50 characters, required</em></span>
             <br></br>
 
             <span class = {style.field}><strong>Date of Birth: </strong></span>
-            <span class = {style.value}>September 7, 1958</span>
+            <input
+                class = {style.value}
+                type = "text"
+                placeholder = "September 7, 1958"
+                maxLength = {50}
+            />
+            <span style={{ color: 'gray' }}><em>  limit 50 characters, optional</em></span>
             <br></br>
 
             <span class = {style.field}><strong>Gender: </strong></span>
-            <span class = {style.value}>Male</span>
+            <input
+                class = {style.value}
+                type = "text"
+                placeholder = "Male"
+                maxLength = {50}
+            />
+            <span style={{ color: 'gray' }}><em>  limit 50 characters, optional</em></span>
             <br></br>
             
 
             <h3 class = {style.header}>Place of Residence</h3>
             
             {/* maybe make address 1 and 2 fields unique headers */}
+            
             <span class = {style.field}><strong>Address 1: </strong></span>
-            <span class = {style.value}>308 Negra Arroyo Lane</span>
+            <input
+                class = {style.value}
+                type = "text"
+                placeholder = "308 Negra Arroyo Lane"
+                maxLength = {100}
+            />
+            <span style={{ color: 'red' }}><em>  limit 100 characters, required</em></span>
             <br></br>
 
             <span class = {style.field}><strong>City: </strong></span>
-            <span class = {style.value}>Albuquerque</span>
+            <input
+                class = {style.value}
+                type = "text"
+                placeholder = "Albuquerque"
+                maxLength = {100}
+            />
+            <span style={{ color: 'red' }}><em>  limit 100 characters, required</em></span>
             <br></br>
+
 
             <span class = {style.field}><strong>State: </strong></span>
-            <span class = {style.value}>New Mexico</span>
+            <select class = {style.value} name = "state" required>
+                <option value = "select-state" required>Select State</option>
+                <option value="AL">AL</option>
+                <option value="AK">AK</option>
+                <option value="AZ">AZ</option>
+                <option value="AR">AR</option>
+                <option value="CA">CA</option>
+                <option value="CO">CO</option>
+                <option value="CT">CT</option>
+                <option value="DE">DE</option>
+                <option value="FL">FL</option>
+                <option value="GA">GA</option>
+                <option value="HI">HI</option>
+                <option value="ID">ID</option>
+                <option value="IL">IL</option>
+                <option value="IN">IN</option>
+                <option value="IA">IA</option>
+                <option value="KS">KS</option>
+                <option value="KY">KY</option>
+                <option value="LA">LA</option>
+                <option value="ME">ME</option>
+                <option value="MD">MD</option>
+                <option value="MA">MA</option>
+                <option value="MI">MI</option>
+                <option value="MN">MN</option>
+                <option value="MS">MS</option>
+                <option value="MO">MO</option>
+                <option value="MT">MT</option>
+                <option value="NE">NE</option>
+                <option value="NV">NV</option>
+                <option value="NH">NH</option>
+                <option value="NJ">NJ</option>
+                <option value="NM">NM</option>
+                <option value="NY">NY</option>
+                <option value="NC">NC</option>
+                <option value="ND">ND</option>
+                <option value="OH">OH</option>
+                <option value="OK">OK</option>
+                <option value="OR">OR</option>
+                <option value="PA">PA</option>
+                <option value="RI">RI</option>
+                <option value="SC">SC</option>
+                <option value="SD">SD</option>
+                <option value="TN">TN</option>
+                <option value="TX">TX</option>
+                <option value="UT">UT</option>
+                <option value="VT">VT</option>
+                <option value="VA">VA</option>
+                <option value="WA">WA</option>
+                <option value="WV">WV</option>
+                <option value="WI">WI</option>
+                <option value="WY">WY</option>
+            </select>
+            <span style={{ color: 'red' }}><em>  required</em></span>
+            <br></br>
+            
+            
+            <span class = {style.field}><strong>Zip Code: </strong></span>
+            <input
+                class = {style.value}
+                type = "text"
+                placeholder="87111"
+                maxLength = {9}
+            />
+            <span style={{ color: 'red' }}><em>  must be 5-9 characters, required</em></span>
             <br></br>
 
-            <span class = {style.field}><strong>Zip Code: </strong></span>
-            <span class = {style.value}>87111</span>
-            <br></br>
+            
 
             
             <br></br>
             <span class = {style.field}><strong>Address 2: </strong></span>
-            <span class = {style.value}>NA</span>
+            <input
+                class = {style.value}
+                type = "text"
+                placeholder = "308 Negra Arroyo Lane"
+                maxLength = {100}
+            />
+            <span style={{ color: 'gray' }}><em>  limit 100 characters, optional</em></span>
             <br></br>
 
             <span class = {style.field}><strong>City: </strong></span>
-            <span class = {style.value}>NA</span>
+            <input
+                class = {style.value}
+                type = "text"
+                placeholder = "Albuquerque"
+                maxLength = {100}
+            />
+            <span style={{ color: 'gray' }}><em>  limit 100 characters, optional</em></span>
             <br></br>
+
 
             <span class = {style.field}><strong>State: </strong></span>
-            <span class = {style.value}>NA</span>
+            <select class = {style.value} name = "state" required>
+                <option value = "select-state" required>Select State</option>
+                <option value="AL">AL</option>
+                <option value="AK">AK</option>
+                <option value="AZ">AZ</option>
+                <option value="AR">AR</option>
+                <option value="CA">CA</option>
+                <option value="CO">CO</option>
+                <option value="CT">CT</option>
+                <option value="DE">DE</option>
+                <option value="FL">FL</option>
+                <option value="GA">GA</option>
+                <option value="HI">HI</option>
+                <option value="ID">ID</option>
+                <option value="IL">IL</option>
+                <option value="IN">IN</option>
+                <option value="IA">IA</option>
+                <option value="KS">KS</option>
+                <option value="KY">KY</option>
+                <option value="LA">LA</option>
+                <option value="ME">ME</option>
+                <option value="MD">MD</option>
+                <option value="MA">MA</option>
+                <option value="MI">MI</option>
+                <option value="MN">MN</option>
+                <option value="MS">MS</option>
+                <option value="MO">MO</option>
+                <option value="MT">MT</option>
+                <option value="NE">NE</option>
+                <option value="NV">NV</option>
+                <option value="NH">NH</option>
+                <option value="NJ">NJ</option>
+                <option value="NM">NM</option>
+                <option value="NY">NY</option>
+                <option value="NC">NC</option>
+                <option value="ND">ND</option>
+                <option value="OH">OH</option>
+                <option value="OK">OK</option>
+                <option value="OR">OR</option>
+                <option value="PA">PA</option>
+                <option value="RI">RI</option>
+                <option value="SC">SC</option>
+                <option value="SD">SD</option>
+                <option value="TN">TN</option>
+                <option value="TX">TX</option>
+                <option value="UT">UT</option>
+                <option value="VT">VT</option>
+                <option value="VA">VA</option>
+                <option value="WA">WA</option>
+                <option value="WV">WV</option>
+                <option value="WI">WI</option>
+                <option value="WY">WY</option>
+            </select>
+            <span style={{ color: 'gray' }}><em>  optional</em></span>
             <br></br>
-
+            
+            
             <span class = {style.field}><strong>Zip Code: </strong></span>
-            <span class = {style.value}>NA</span>
+            <input
+                class = {style.value}
+                type = "text"
+                placeholder="87111"
+                maxLength = {9}
+            />
+            <span style={{ color: 'gray' }}><em>  must be 5-9 characters, optional</em></span>
             <br></br>
 
 
@@ -68,6 +241,20 @@ const AccountDetails = () => {
             <h3 class = {style.header}>Other Information</h3>
             
             <span class = {style.field}><strong>Skills: </strong></span>
+            {/* put multi select menu here */}
+
+            <select class = {style.value} name = "skill" multiple required>
+                <option value = "select-skill" required>Select Skill</option>
+                <option value = "communicate" required>Good Communicator</option>
+                <option value = "strong" required>Strong</option>
+                <option value = "creative" required>Creativity</option>
+                <option value = "sales" required>Sales</option>
+                <option value = "problem-solver" required>Problem Solver</option>
+            </select>
+            <span style={{ color: 'red' }}><em>  required</em></span>
+            <br></br>
+
+
             <span class = {style.value}>Chemistry, Baking, Car Washes</span>
             <br></br>
 

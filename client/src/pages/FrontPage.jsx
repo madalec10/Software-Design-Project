ae35dec0f2d7895a3e17d2b58d320b4907ddff04
Ethan B <ethan.bydalek@gmail.com>
2025-10-04 20:28:10 -0500

    cleaned up FrontPage.jsx code

--- client/src/pages/FrontPage.jsx
+++ client/src/pages/FrontPage.jsx
@@ -1,78 +1,71 @@
-import React from 'react'
-import style from './FrontPage.module.css'
+import React from 'react';
+import './FrontPage.css';
 
-// is there a way I can import images from a single directory instead of this
-import africaVolunteerImg from "../assets/africa-volunteer.png"
-import africanPeopleImg from "../assets/african-people.jpg"
-import diversityImg from "../assets/diversity.jpg"
-import indiaVolunteerImg from "../assets/india-volunteer.jpg"
-import languageExchangeImg from "../assets/language-exchange.jpg"
-import diverseFriendsImg from "../assets/diverse-friends.jpg"
-import plantVolunteerImg from "../assets/plant-volunteer.jpg"
-import logoImg from "../assets/logo.jpg"
+import africaVolunteerImg from "../assets/africa-volunteer.png";
+import africanPeopleImg from "../assets/african-people.jpg";
+import diversityImg from "../assets/diversity.jpg";
+import indiaVolunteerImg from "../assets/india-volunteer.jpg";
+import languageExchangeImg from "../assets/language-exchange.jpg";
+import diverseFriendsImg from "../assets/diverse-friends.jpg";
+import logoImg from "../assets/logo.jpg";
 
- 
 const FrontPage = () => {
     console.log("FrontPage")
  
     return(
         <div>
-            {/* FIXME: this link is temporary. should replace the login/signup links at the nav bar when user already signed in */}
-            <a href="/account-details" rel="noopener noreferrer" class = {style.accountLink}>Account Details</a> <br></br>
+            {/* removed classes from all images */}
+            <img src={logoImg} alt="helping hands logo"></img>
+            <h1 className="logoText">Helping Hands</h1>
             
-            <img src={logoImg} class = {style.logoImg} alt="helping hands logo" width="200" height="200"></img>
-            {/* FIXME: place helping hands title next to the logo */}
-            <h1 class = {style.logoText}>Helping Hands</h1>
-            
-            <p class = {style.motto}><em>Nurturing a better future</em></p>
+            <p className="motto">Nurturing a better future</p>
 
             
-            <img src={diversityImg} class = {style.bigImg} alt="diverse group of people" width="512" height="341"></img>
-            <br></br>
-            <h1 class = {style.header}>About Us</h1>
-            <p class = {style.body}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Impedit maxime modi et harum, sint repellat repellendus laudantium 
-                laboriosam eaque accusantium beatae, minus laborum, qui molestiae. 
-                Necessitatibus perspiciatis ullam modi vitae.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Impedit maxime modi et harum, sint repellat repellendus laudantium 
-                laboriosam eaque accusantium beatae, minus laborum, qui molestiae. 
-                Necessitatibus perspiciatis ullam modi vitae.
+            <img src={diversityImg} alt="diverse group of people"></img>
+            
+
+            <h1 className="header">About Us</h1>
+
+            <p className="body">
+                At Helping Hands, we believe in the value of helping others. Since 1970, we have
+                helped over 4 people across 30 states and counting. Today, we've grown into a vast
+                network of like minded people that are eager to lend a helping hand to those in
+                need. Through our online platform, we can easily connect volunteers to events that 
+                match their skills and interests like never before -- so don't be a stranger and lend a helping hand today!
             </p>
-            {/* <br></br> */}
-            <h1 class = {style.header}>Mission Statement</h1>
-            <p class = {style.body}>
-                <p class = {style.mission}>Our mission is simple: <strong>To leave the world a better place than we found it.</strong></p>
-                <br></br>
+
+
+            <h1 className="header">Mission Statement</h1>
+            
+            <p className="mission">Our mission is simple: To leave the world a better place than we found it.</p>
+            <p className="body">
                 Helping Hands seeks to improve the lives of underpriveleged communities in
                 health, education, and hapiness while fostering an ever-expanding community.  
                 We strive to connect people like never before, and help everyone for a better future. 
-                
             </p>
-            <h1 class = {style.header}>Gallery</h1>
-            {/* flexbox */}
 
-            <div class = {style.row1}>
-                <img src={africaVolunteerImg} class = {style.galleryImg} alt="volunteers in africa" width="480" height="250"></img>
-                <img src={africanPeopleImg} class = {style.galleryImg} alt="african people" width="466" height="311"></img>
+            
+            <h1 className="header">Gallery</h1>
+
+
+            {/* could row1 and row2 be just one class */}
+            <div className="row1">
+                <img src={africaVolunteerImg} alt="volunteers in africa"></img>
+                <img src={africanPeopleImg} alt="african people"></img>
             </div>
 
-            <div class = {style.row2}>
-                <img src={indiaVolunteerImg} class = {style.galleryImg} alt="volunteers in india" width="474" height="291"></img>
-                <img src={languageExchangeImg} class = {style.galleryImg} alt="language exchange event" width="512" height="341"></img>
+            <div className="row2">
+                <img src={indiaVolunteerImg} alt="volunteers in india"></img>
+                <img src={languageExchangeImg} alt="language exchange event"></img>
             </div>
             
-            {/* <br></br> */}
+
+
+            <h1 className="askJoin">Lend a Helping Hand Today!</h1>
+
+            <img src={diverseFriendsImg} alt="diverse group of friends"></img>
             
-            {/* <br></br> */}
-            {/* <img src={plantVolunteerImg} class = {style.galleryImg} alt="volunteering by planting plants" width="600" height="315"></img> */}
-
-
-            <h1 class = {style.askJoin}>Lend a Helping Hand Today!</h1>
-
-            <img src={diverseFriendsImg} class = {style.bigImg} alt="diverse group of friends" with="915" height="400"></img>
-            <p class = {style.enterAccount}><a href="/log-in">Login</a> or <a href="/sign-up">Create an Account</a>!</p>
+            <p className="enterAccount"><a href="/log-in">Login</a> or <a href="/sign-up">Create an Account</a>!</p>
         </div>
         
 

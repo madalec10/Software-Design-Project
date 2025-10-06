92a4246a54f121dd373853f12c5251362d7c036f
Ethan B <ethan.bydalek@gmail.com>
2025-10-06 12:01:06 -0500

    WIP: css finished*
    *needs more details of events: required skills, urgency, and replace lorem description text

--- client/src/pages/VolunteerHistory.jsx
+++ client/src/pages/VolunteerHistory.jsx
@@ -36,9 +36,13 @@ const VolunteerHistory = () => {
                     <div className="beach">
                         <h1 className="header">Beach Clean Up</h1>
 
-                        <span className="date">October 14, 2025</span>
-                        <span className="time">3:40 PM</span>
-                        <span className="location">Galveston Beach</span>
+                        <div className="textLine">
+                           <span className="date">October 14, 2025</span>
+                            <span> • </span>
+                            <span className="time">3:40 PM</span>
+                            <span> • </span>
+                            <span className="location">Galveston Beach</span>
+                        </div>
 
                         <p className="description">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -56,9 +60,13 @@ const VolunteerHistory = () => {
                     <div className="coal">
                         <h1 className="header">Kids Coal Drive</h1>
 
-                        <span className="date">December 5, 2025</span>
-                        <span className="time">9:00 AM</span>
-                        <span className="location">Downtown Ohio Fire Department</span>
+                        <div className="textLine">
+                            <span className="date">December 5, 2025</span>
+                            <span> • </span>
+                            <span className="time">9:00 AM</span>
+                            <span> • </span>
+                            <span className="location">Downtown Ohio Fire Department</span>
+                        </div>
 
                         <p className="description">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -76,9 +84,13 @@ const VolunteerHistory = () => {
                     <div className="tree">
                         <h1 className="header">Tree Planting</h1>
 
-                        <span className="date">October 18, 2025</span>
-                        <span className="time">2:30 PM</span>
-                        <span className="location">Rincon National Park</span>
+                        <div className="textLine">
+                            <span className="date">October 18, 2025</span>
+                            <span> • </span>
+                            <span className="time">2:30 PM</span>
+                            <span> • </span>
+                            <span className="location">Rincon National Park</span>
+                        </div>
 
                         <p className="description">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -96,9 +108,13 @@ const VolunteerHistory = () => {
                     <div className="nurse">
                         <h1 className="header">Nursing Homes</h1>
 
-                        <span className="date">November 14, 2025</span>
-                        <span className="time">11:00 AM</span>
-                        <span className="location">Gotham Nursing Homes</span>
+                        <div className="textLine">
+                            <span className="date">November 14, 2025</span>
+                            <span> • </span>
+                            <span className="time">11:00 AM</span>
+                            <span> • </span>
+                            <span className="location">Gotham Nursing Homes</span>
+                        </div>
 
                         <p className="description">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -116,9 +132,13 @@ const VolunteerHistory = () => {
                     <div className="feral">
                         <h1 className="header">Animal Shelter</h1>
 
-                        <span className="date">January 8, 2026</span>
-                        <span className="time">4:30 PM</span>
-                        <span className="location">International Feral Animal Shelter</span>
+                        <div className="textLine">
+                            <span className="date">January 8, 2026</span>
+                            <span> • </span>
+                            <span className="time">4:30 PM</span>
+                            <span> • </span>
+                            <span className="location">International Feral Animal Shelter</span>
+                        </div>
 
                         <p className="description">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -136,9 +156,13 @@ const VolunteerHistory = () => {
                     <div className="soup">
                         <h1 className="header">Soup Kitchen</h1>
 
-                        <span className="date">February 12, 2026</span>
-                        <span className="time">1:30 PM</span>
-                        <span className="location">Missouri Coca-Cola Factory</span>
+                        <div className="textLine">
+                            <span className="date">February 12, 2026</span>
+                            <span> • </span>
+                            <span className="time">1:30 PM</span>
+                            <span> • </span>
+                            <span className="location">Missouri Coca-Cola Factory</span>
+                        </div>
 
                         <p className="description">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -156,9 +180,13 @@ const VolunteerHistory = () => {
                     <div className="lebron">
                         <h1 className="header">Special Needs School</h1>
 
-                        <span className="date">October 20, 2025</span>
-                        <span className="time">4:30 PM</span>
-                        <span className="location">Lebron James School for Special Needs</span>
+                        <div className="textLine">
+                            <span className="date">October 20, 2025</span>
+                            <span> • </span>
+                            <span className="time">4:30 PM</span>
+                            <span> • </span>
+                            <span className="location">Lebron James School for Special Needs</span>
+                        </div>
 
                         <p className="description">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. 

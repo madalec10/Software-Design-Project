16c5425fc726b3c221bcb189d453051299a12182
Ethan B <ethan.bydalek@gmail.com>
2025-10-06 19:00:23 -0500

    added -VH to all classes

--- client/src/pages/VolunteerHistory.jsx
+++ client/src/pages/VolunteerHistory.jsx
@@ -24,33 +24,33 @@ const VolunteerHistory = () => {
     return(
         <div>
             {/* PAGE TITLE */}
-            <div className="parentTitle">
-                <div className="title">
-                    <h1 className="pageName">Volunteer History</h1>
+            <div className="parentTitleVH">
+                <div className="titleVH">
+                    <h1 className="pageNameVH">Volunteer History</h1>
                 </div>
             </div>
             
             {/* BEACH */}
-            <div className="parent">
-                <div className="child">
-                    <div className="beach">
-                        <h1 className="header">Beach Clean Up</h1>
+            <div className="parentVH">
+                <div className="childVH">
+                    <div className="beachVH">
+                        <h1 className="headerVH">Beach Clean Up</h1>
 
-                        <div className="textLine">
-                            <span className="date">October 14, 2024</span>
+                        <div className="textLineVH">
+                            <span className="dateVH">October 14, 2024</span>
                             <span> • </span>
-                            <span className="time">3:40 PM</span>
+                            <span className="timeVH">3:40 PM</span>
                             <span> • </span>
-                            <span className="location">Galveston Beach</span>
+                            <span className="locationVH">Galveston Beach</span>
                         </div>
 
-                        <div className="textLine">
-                            <span className="skills">Skill(s) Needed: Organizing, Teamwork</span>
+                        <div className="textLineVH">
+                            <span className="skillsVH">Skill(s) Needed: Organizing, Teamwork</span>
                             <span> • </span>
-                            <span className="urgency">Help Necessary</span>
+                            <span className="urgencyVH">Help Necessary</span>
                         </div>
 
-                        <p className="description">
+                        <p className="descriptionVH">
                             Come on over to Galveston Beach to help clean up 
                             trash with your fellow volunteers! Plastic bags will 
                             be provided for participants to fill trash with. Afterwards, 
@@ -62,26 +62,26 @@ const VolunteerHistory = () => {
             </div>
 
             {/* COAL */}
-            <div className="parent">
-                <div className="child">
-                    <div className="coal">
-                        <h1 className="header">Kids Coal Drive</h1>
+            <div className="parentVH">
+                <div className="childVH">
+                    <div className="coalVH">
+                        <h1 className="headerVH">Kids Coal Drive</h1>
 
-                        <div className="textLine">
-                            <span className="date">December 5, 2024</span>
+                        <div className="textLineVH">
+                            <span className="dateVH">December 5, 2024</span>
                             <span> • </span>
-                            <span className="time">9:00 AM</span>
+                            <span className="timeVH">9:00 AM</span>
                             <span> • </span>
-                            <span className="location">Downtown Ohio Fire Department</span>
+                            <span className="locationVH">Downtown Ohio Fire Department</span>
                         </div>
 
-                        <div className="textLine">
-                            <span className="skills">Skill(s) Needed: None</span>
+                        <div className="textLineVH">
+                            <span className="skillsVH">Skill(s) Needed: None</span>
                             <span> • </span>
-                            <span className="urgency">Help Needed</span>
+                            <span className="urgencyVH">Help Needed</span>
                         </div>
 
-                        <p className="description">
+                        <p className="descriptionVH">
                             Calling all volunteers! Mr. Kringle needs your help this 
                             holiday season! Using the official North Pole naughty list 
                             provided by the man himself, we will be organizing coals by 
@@ -93,26 +93,26 @@ const VolunteerHistory = () => {
             </div>
 
             {/* TREE */}
-            <div className="parent">
-                <div className="child">
-                    <div className="tree">
-                        <h1 className="header">Tree Planting</h1>
+            <div className="parentVH">
+                <div className="childVH">
+                    <div className="treeVH">
+                        <h1 className="headerVH">Tree Planting</h1>
 
-                        <div className="textLine">
-                            <span className="date">October 18, 2024</span>
+                        <div className="textLineVH">
+                            <span className="dateVH">October 18, 2024</span>
                             <span> • </span>
-                            <span className="time">2:30 PM</span>
+                            <span className="timeVH">2:30 PM</span>
                             <span> • </span>
-                            <span className="location">Rincon National Park</span>
+                            <span className="locationVH">Rincon National Park</span>
                         </div>
 
-                        <div className="textLine">
+                        <div className="textLineVH">
                             <span className="skills">Skill(s) Needed: Time Management</span>
                             <span> • </span>
                             <span className="urgency">Help Would be Appreciated</span>
                         </div>
 
-                        <p className="description">
+                        <p className="descriptionVH">
                             Come together this October in nurturing a better future 
                             for mother nature. In honor of National Rincon Day, we will 
                             be planting 3,360 trees around Rincon National Park. Great 
@@ -123,26 +123,26 @@ const VolunteerHistory = () => {
             </div>
 
             {/* NURSE */}
-            <div className="parent">
-                <div className="child">
-                    <div className="nurse">
-                        <h1 className="header">Nursing Homes</h1>
+            <div className="parentVH">
+                <div className="childVH">
+                    <div className="nurseVH">
+                        <h1 className="headerVH">Nursing Homes</h1>
 
-                        <div className="textLine">
-                            <span className="date">November 14, 2024</span>
+                        <div className="textLineVH">
+                            <span className="dateVH">November 14, 2024</span>
                             <span> • </span>
-                            <span className="time">11:00 AM</span>
+                            <span className="timeVH">11:00 AM</span>
                             <span> • </span>
-                            <span className="location">Gotham Nursing Homes</span>
+                            <span className="locationVH">Gotham Nursing Homes</span>
                         </div>
 
-                        <div className="textLine">
+                        <div className="textLineVH">
                             <span className="skills">Skill(s) Needed: Communication</span>
                             <span> • </span>
                             <span className="urgency">Help Necessary</span>
                         </div>
 
-                        <p className="description">
+                        <p className="descriptionVH">
                             While rounding up the gang loose at Arkham Asylum, the 
                             Dark Knight needs your help rounding up the elderly
                             in the nursing homes for a good time! Help facilitate 
@@ -154,26 +154,26 @@ const VolunteerHistory = () => {
             </div>
 
             {/* FERAL */}
-            <div className="parent">
-                <div className="child">
-                    <div className="feral">
-                        <h1 className="header">Animal Shelter</h1>
+            <div className="parentVH">
+                <div className="childVH">
+                    <div className="feralVH">
+                        <h1 className="headerVH">Animal Shelter</h1>
 
-                        <div className="textLine">
-                            <span className="date">January 8, 2025</span>
+                        <div className="textLineVH">
+                            <span className="dateVH">January 8, 2025</span>
                             <span> • </span>
-                            <span className="time">4:30 PM</span>
+                            <span className="timeVH">4:30 PM</span>
                             <span> • </span>
-                            <span className="location">International Feral Animal Shelter</span>
+                            <span className="locationVH">International Feral Animal Shelter</span>
                         </div>
 
-                        <div className="textLine">
+                        <div className="textLineVH">
                             <span className="skills">Skill(s) Needed: Power Tools</span>
                             <span> • </span>
                             <span className="urgency">Help Would be Appreciated</span>
                         </div>
 
-                        <p className="description">
+                        <p className="descriptionVH">
                             The staff at the IFAS are looking into toys and structures 
                             to help relieve the stress of their residents! Join them on 
                             January of next year to create wooden climbing structures. 
@@ -185,26 +185,26 @@ const VolunteerHistory = () => {
             </div>
 
             {/* SOUP */}
-            <div className="parent">
-                <div className="child">
-                    <div className="soup">
-                        <h1 className="header">Soup Kitchen</h1>
+            <div className="parentVH">
+                <div className="childVH">
+                    <div className="soupVH">
+                        <h1 className="headerVH">Soup Kitchen</h1>
 
-                        <div className="textLine">
-                            <span className="date">February 12, 2025</span>
+                        <div className="textLineVH">
+                            <span className="dateVH">February 12, 2025</span>
                             <span> • </span>
-                            <span className="time">1:30 PM</span>
+                            <span className="timeVH">1:30 PM</span>
                             <span> • </span>
-                            <span className="location">Missouri Coca-Cola Factory</span>
+                            <span className="locationVH">Missouri Coca-Cola Factory</span>
                         </div>
 
-                        <div className="textLine">
+                        <div className="textLineVH">
                             <span className="skills">Skill(s) Needed: Communication, Time Management</span>
                             <span> • </span>
                             <span className="urgency">Help Needed</span>
                         </div>
 
-                        <p className="description">
+                        <p className="descriptionVH">
                             The Cola-Cola Company is looking to get into the 
                             soup industry and also happens to be in a giving mood. 
                             Come volunteer for the local Missouri community in 
@@ -216,26 +216,26 @@ const VolunteerHistory = () => {
             </div>
 
             {/* LEBRON */}
-            <div className="parent">
-                <div className="child">
-                    <div className="lebron">
-                        <h1 className="header">Special Needs School</h1>
+            <div className="parentVH">
+                <div className="childVH">
+                    <div className="lebronVH">
+                        <h1 className="headerVH">Special Needs School</h1>
 
-                        <div className="textLine">
-                            <span className="date">October 20, 2024</span>
+                        <div className="textLineVH">
+                            <span className="dateVH">October 20, 2024</span>
                             <span> • </span>
-                            <span className="time">4:30 PM</span>
+                            <span className="timeVH">4:30 PM</span>
                             <span> • </span>
-                            <span className="location">Lebron James School for Special Needs</span>
+                            <span className="locationVH">Lebron James School for Special Needs</span>
                         </div>
 
-                        <div className="textLine">
+                        <div className="textLineVH">
                             <span className="skills">Skill(s) Needed: Communication</span>
                             <span> • </span>
                             <span className="urgency">Help Necessary</span>
                         </div>
 
-                        <p className="description">
+                        <p className="descriptionVH">
                             Come join us in the grand opening of Lebron James' special 
                             needs school. Volunteers will give tours around the campus 
                             and facilitate with delivering the end of day kindergarten 
